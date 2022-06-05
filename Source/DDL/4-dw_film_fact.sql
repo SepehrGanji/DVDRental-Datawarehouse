f11d98c7a5974c7aa42d6feb37ab87d479f83429
SepehrGanji <sepehrganji79@gmail.com>
2022-06-05 10:57:04 +0430

    add film-mart facts

---
+++ Source/DDL/4-dw_film_fact.sql
@@ -0,0 +1,43 @@
+CREATE TABLE dw.fact_trans_film
+(
+    film integer NOT NULL,
+    category integer NOT NULL,
+    year integer NOT NULL,
+    language integer NOT NULL,
+    rating integer NOT NULL,
+    rate numeric(5, 2) NOT NULL,
+    length smallint NOT NULL
+);
+
+ALTER TABLE IF EXISTS dw.fact_trans_film
+    OWNER to postgres;
+
+----
+
+CREATE TABLE dw.fact_yearly_category
+(
+    category integer NOT NULL,
+    year integer NOT NULL,
+    film_count integer NOT NULL,
+    rate_avg numeric(5, 2) NOT NULL,
+    length_avg numeric(5, 2) NOT NULL
+);
+
+ALTER TABLE IF EXISTS dw.fact_yearly_category
+    OWNER to postgres;
+
+----
+
+CREATE TABLE dw.fact_acc_category
+(
+    category integer NOT NULL,
+    film_count integer NOT NULL,
+    rate_avg numeric(5, 2) NOT NULL,
+    length_avg numeric(5, 2) NOT NULL
+);
+
+ALTER TABLE IF EXISTS dw.fact_acc_category
+    OWNER to postgres;
+
+----
+
