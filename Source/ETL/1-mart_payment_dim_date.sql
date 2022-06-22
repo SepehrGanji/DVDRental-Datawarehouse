e1e998f7b7eb37055a30bacc6cc995149e7d1928
sobhan.saf79 <sobhan.saf79@gmail.com>
2022-06-22 13:59:07 +0430

    added a procedure for automatically date insert
    also a bug fixed

--- Source/ETL/1-mart_payment_dim_date.sql
+++ Source/ETL/1-mart_payment_dim_date.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE fill_payment_dim_date(start_date DATE, number_of_days INT)
+CREATE PROCEDURE dw.fill_payment_dim_date(start_date DATE, number_of_days INT)
 LANGUAGE plpgsql AS
 $$
 DECLARE 
@@ -8,7 +8,7 @@ BEGIN
     i := 0;
     cur_date := start_date;
     WHILE i < number_of_days LOOP
-        INSERT INTO dw.dim_date(year, month, day) 
+        INSERT INTO dw.dim_date(day, month, year) 
             VALUES (DATE_PART('day', cur_date), DATE_PART('month', cur_date), DATE_PART('year', cur_date));
         i := i + 1;
         cur_date := cur_date + 1;
@@ -16,3 +16,25 @@ BEGIN
 END
 $$;
 
+CREATE PROCEDURE dw.fill_date_from_last_available(number_of_days INT)
+LANGUAGE plpgsql AS
+$$
+DECLARE
+    last_available DATE;
+BEGIN
+    DROP TABLE IF EXISTS tmp1; 
+    CREATE TABLE tmp1(
+        date_ DATE
+    );
+    INSERT INTO tmp1
+        SELECT TO_DATE(CONCAT(day::text, '-', month::text, '-', year::text), 'DD-MM-YYYY')
+        FROM dw.dim_date;
+    IF EXISTS (SELECT * FROM tmp1) THEN
+        SELECT MAX(date_) INTO last_available FROM tmp1;
+    ELSE
+        SELECT MIN(payment_date) INTO last_available FROM public.payment;
+    END IF;
+    last_available := last_available + INTERVAL '1 day';
+    CALL fill_payment_dim_date(last_available, number_of_days);
+END
+$$;
