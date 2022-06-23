76b0fa62fb3cb3419a1ea0925b3014d6fc498d79
sobhan.saf79 <sobhan.saf79@gmail.com>
2022-06-23 11:23:52 +0430

    reset

---
+++ Source/ETL/Payment/8-fill_tables_first_time.sql
@@ -0,0 +1,7 @@
+call dw.fill_date_from_last_available(365);
+call dw.fill_countries();
+call dw.fill_staff();
+call dw.fill_sotre();
+call dw.fill_customer();
+call dw.fill_film();
+call dw.fill_fact_trans_payment();
