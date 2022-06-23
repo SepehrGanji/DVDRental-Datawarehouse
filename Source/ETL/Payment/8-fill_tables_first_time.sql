b03c0501c3f588137c6e8e4c4df25725e6a0a93a
sobhan.saf79 <sobhan.saf79@gmail.com>
2022-06-23 13:14:15 +0430

    some renaming

--- Source/ETL/Payment/8-fill_tables_first_time.sql
+++
@@ -1,7 +0,0 @@
-call dw.fill_date_from_last_available(365);
-call dw.fill_countries();
-call dw.fill_staff();
-call dw.fill_sotre();
-call dw.fill_customer();
-call dw.fill_film();
-call dw.fill_fact_trans_payment();
