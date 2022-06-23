637ce0bf5d851f179c945bb9a12af3d13a59a480
sobhan.saf79 <sobhan.saf79@gmail.com>
2022-06-23 13:32:06 +0430

    procedures to fill facts are added

---
+++ Source/ETL/Payment/10-procedures.sql
@@ -0,0 +1,46 @@
+CREATE PROCEDURE fill_tables_first_time()
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    from_date date;
+    to_date date;
+
+BEGIN
+    SELECT MIN(payment_date)::date INTO from_date FROM payment;
+    SELECT MAX(payment_date)::date INTO to_date FROM payment;
+
+    call dw.fill_date_from_last_available(100000);
+    call dw.fill_countries();
+    call dw.fill_staff();
+    call dw.fill_sotre();
+    call dw.fill_customer();
+    call dw.fill_film();
+    call dw.fill_fact_trans_payment();
+    call dw.fill_fact_daily_payment(from_date, to_date);
+    call dw.fill_fact_acc_payment();
+END
+$$;
+
+CREATE PROCEDURE fill_tables()
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    from_date date;
+    to_date date;
+
+BEGIN
+    SELECT last_update INTO from_date FROM dw.fact_daily_payment_last_update;
+    SELECT MAX(payment_date)::date INTO to_date FROM payment;
+
+    call dw.fill_countries();
+    call dw.fill_staff();
+    call dw.fill_sotre();
+    call dw.fill_customer();
+    call dw.fill_film();
+    call dw.fill_fact_trans_payment();
+    call dw.fill_fact_daily_payment(from_date, to_date);
+    call dw.fill_fact_acc_payment();
+END
+$$
