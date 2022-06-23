5da0feabc91f6af38d16a56f0f09f8c26824703b
sobhan.saf79 <sobhan.saf79@gmail.com>
2022-06-23 13:45:08 +0430

    logging for daily fact procedure

--- Source/ETL/Payment/8-mart_payment_fact_daily_payment.sql
+++ Source/ETL/Payment/8-mart_payment_fact_daily_payment.sql
@@ -38,11 +38,19 @@ DECLARE
 BEGIN
     curr_date := from_date;
 
+     INSERT INTO dw.mart_payment_logs(filling_table, time_occured, description)
+        VALUES ('fact_daily_payment', CURRENT_TIMESTAMP, 'started to add records from ' || from_date || ' to ' || to_date);
+
+
     INSERT INTO dw.tmp_fact_daily_payment_2
         SELECT dim_customer.id, dim_store.id
         FROM dw.dim_customer CROSS JOIN dw.dim_store;
 
     WHILE curr_date < to_date do LOOP
+
+        INSERT INTO dw.mart_payment_logs(filling_table, time_occured, description)
+            VALUES ('fact_daily_payment', CURRENT_TIMESTAMP, 'adding data of ' || curr_date);
+
         SELECT id FROM dw.dim_date INTO curr_date_id
         WHERE DATE_PART('day', curr_date) = dim_date.day
             AND DATE_PART('month', curr_date) = dim_date.month
@@ -55,6 +63,8 @@ BEGIN
             FROM dw.fact_trans_payment
             WHERE date=curr_date_id;
         
+        INSERT INTO dw.mart_payment_logs(filling_table, time_occured, description)
+            VALUES ('fact_daily_payment', CURRENT_TIMESTAMP, 'the related day data added to tmp table');
 
         TRUNCATE TABLE dw.tmp_fact_daily_payment_3;
         INSERT INTO dw.tmp_fact_daily_payment_3(date, customer, store, amount)
@@ -63,14 +73,23 @@ BEGIN
                 ON tmp2.customer_id = tmp1.customer AND tmp2.store_id = tmp1.store
             WHERE tmp1.date IS NULL AND tmp1.customer IS NULL AND tmp1.film IS NULL;
 
+        INSERT INTO dw.mart_payment_logs(filling_table, time_occured, description)
+            VALUES ('fact_daily_payment', CURRENT_TIMESTAMP, 'customers not buying from a store added to tmp table');
+
         INSERT INTO dw.tmp_fact_daily_payment_3
             SELECT curr_date_id, customer, store, SUM(amount)
             FROM dw.tmp_fact_daily_payment_1
             GROUP BY customer, store;
+
+        INSERT INTO dw.mart_payment_logs(filling_table, time_occured, description)
+            VALUES ('fact_daily_payment', CURRENT_TIMESTAMP, 'customers having transaction added to tmp table');
         
         INSERT INTO dw.fact_daily_payment(date, customer, store, amount)
             SELECT date, customer, store, amount
             FROM dw.tmp_fact_daily_payment_3;
+
+        INSERT INTO dw.mart_payment_logs(filling_table, time_occured, description)
+            VALUES ('fact_daily_payment', CURRENT_TIMESTAMP, 'new data added to fact');
 
         curr_date := curr_date + INTERVAL '1 day';
 
