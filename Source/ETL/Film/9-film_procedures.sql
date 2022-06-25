59ddbc9312bb2ea0cd63619fd6c5ad091cc43d73
SepehrGanji <sepehrganji79@gmail.com>
2022-06-25 22:08:22 +0430

    minor fix

--- Source/ETL/Film/9-film_procedures.sql
+++ Source/ETL/Film/9-film_procedures.sql
@@ -2,9 +2,6 @@ CREATE PROCEDURE dw.fill_film_tables_first_time()
 LANGUAGE plpgsql
 AS
 $$
-DECLARE
-    from_year date;
-    to_year date;
 BEGIN
     call dw.fill_film_dim_year(2000, 22);
     call dw.fill_categories();
@@ -12,18 +9,15 @@ BEGIN
     call dw.fill_ratings();
     call dw.fill_fact_trans_film();
     call dw.fill_factless_film_category();
-    call dw.fill_fact_yearly_category(from_year, to_year);
+    call dw.fill_fact_yearly_category(2000, 2022);
     call dw.fill_fact_acc_category();
 END
 $$;
 
-CREATE PROCEDURE dw.fill_film_tables()
+CREATE PROCEDURE dw.fill_film_tables(from_year INT, to_year INT)
 LANGUAGE plpgsql
 AS
 $$
-DECLARE
-    from_year date;
-    to_year date;
 BEGIN
     call dw.fill_categories();
     call dw.fill_languages();
